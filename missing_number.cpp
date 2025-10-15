e31dcac0ef731e8d03e3c7a95c3c63050a8888d8
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-15 23:49:22 +0530

    latest commit:

---
+++ missing_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int findMissingNumber(int arr[], int n) {
+    int total = (n + 1) * (n + 2) / 2;  
+    for (int i = 0; i < n; i++) {
+        total -= arr[i];
+    }
+    return total;
+}
+
+int main() {
+    int arr[] = {1, 2, 4, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Missing number: " << findMissingNumber(arr, n);
+    return 0;
+}
