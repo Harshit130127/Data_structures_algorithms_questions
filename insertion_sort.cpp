0c7342f9d539d4e2e44dec2a8f53e261591b8e1f
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-24 17:22:17 +0530

    insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -6,11 +6,15 @@ void insertion_sort(int arr[], int n){
 
     for(int i=1;i<n;i++){
 
-        {
+        
 for(int j=i;j>0;j--){
 
             if(arr[j]<arr[j-1]){
-                swap(arr[j],arr[j-1]);
+                // swap(arr[j],arr[j-1]);
+
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
             }
             else{
                 break;
@@ -24,7 +28,7 @@ for(int j=i;j>0;j--){
 
 
     
-}
+
 
 
 int main(){
