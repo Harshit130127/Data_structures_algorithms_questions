57fb0bfb473ab7f623bffa2e9dd2204f798e527a
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-25 21:14:23 +0530

    first occurence

---
+++ first_position_of_element.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+void binary_search_first_position(int arr[], int n, int key) {   // to perform binary search
+    int start = 0;
+    int end = n - 1;
+    int first=-1;
+    int last=-1;
+    while(start <= end) {
+        int mid = (start + end) / 2;
+
+        if(arr[mid] == key) {
+            first=mid;     // here first is used to store the first occurrence index
+            end=mid-1;
+
+        }
+        else if(arr[mid] < key) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+    
+    cout << "Element not found in the array." << endl;
+}
+
+
+int main() {
+
+    int arr[1000], n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    binary_search_first_position(arr, n, key);
+
+    return 0;
+}
