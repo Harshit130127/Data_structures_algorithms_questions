f922b92a646bd35dc3f1a12fc017ba1768031f77
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-28 18:44:05 +0530

    latest commit

--- minimum_element_rotated_array.cpp
+++ minimum_element_rotated_array.cpp
@@ -1,45 +1,27 @@
 #include<iostream>
-
 using namespace std;
 
-int minimum_element_index_in_rotated_array(int arr[],int n){
+int minimum_element_index_in_rotated_array(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
 
-int ans=arr[0];
-int start=0;
+    while (start < end) {
+        int mid = start + (end - start) / 2;
 
-int end=n-1;
-
-while(start<=end)
-
-{
-int mid=(start+(end-start))/2;
-
-    if(arr[mid]>=arr[0]){
-        start=mid+1;
+        if (arr[mid] > arr[end]) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
     }
-
-    else{
-
-        ans=arr[mid];
-        end=mid-1;
-    }
-
-}
-return ans;
-
+    return arr[start];
 }
 
-int main(){
+int main() {
+    int arr[] = {5, 6, 7, 1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-
-int arr[]={5,6,7,1,2,3,4};
-
-
-int n=sizeof(arr)/sizeof(arr[0]);
-
-cout<<"The minimum element in the rotated sorted array is: ";
-
-int element=minimum_element_index_in_rotated_array(arr,n);
-cout<<element<<endl;
-
+    cout << "The minimum element in the rotated sorted array is: ";
+    int element = minimum_element_index_in_rotated_array(arr, n);
+    cout << element << endl;
 }
