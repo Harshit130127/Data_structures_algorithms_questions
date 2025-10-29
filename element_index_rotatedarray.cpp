fab90d83ca7fa7b0788445c7a8d473b91073d78e
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-29 10:28:26 +0530

    latest commit

--- element_index_rotatedarray.cpp
+++ element_index_rotatedarray.cpp
@@ -1,47 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int element_search(int arr[], int n, int target) {
+    int start = 0;
+    int end = n - 1;
 
-int element_search(int arr[],int n,int target)
-{
+    while(start <= end) {
+        int mid = start + (end - start) / 2;
 
-int start=0;
-int end=n-1;
-
-int ans=-1;
-
-while(start<=end){
-
-int mid=(start+(end-start))/2;
-
-
-if(arr[mid]==target){
-    ans=mid;
-    return mid;
+        if(arr[mid] == target) {
+            return mid; // return index not value
+        }
+        else if(arr[mid] >= arr[start]) {  // left half is sorted
+            if(arr[start] <= target && target < arr[mid])  // target in left half
+                end = mid - 1;
+            else    
+                start = mid + 1;   // target in right half
+        }
+        else {  // right half is sorted
+            if  (arr[mid] < target && target <= arr[end])  // target in right half
+                start = mid + 1;
+            else  // target in left half
+                end = mid - 1;
+        }
+    }
+    return -1;
 }
 
-else if(arr[mid]>=arr[0]){
+int main() {
+    int n;
+    cout << "Enter the size of array" << endl;
+    cin >> n;
 
-    if(arr[start]<=target && arr[mid]>=target)
-end=mid-1;
+    int arr[n];
+    cout << "Enter the elements of array" << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-else
-start=mid+1;
+    int target;
+    cout << "Enter the target element" << endl;
+    cin >> target;
 
-}
+    int result = element_search(arr, n, target);
 
-else{
-if(arr[mid]<=target && arr[mid]>=target){
-
-    start=mid+1;
-}
-  else  end=mid-1;
-}
-
-
-
-
-}
-
-
+    if(result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
 }
