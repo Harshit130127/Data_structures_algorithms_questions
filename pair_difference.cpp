b4ecfcb9358b3e84e47618a8472aa168eb82a7e7
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-12-15 21:50:45 +0530

    latest commit

---
+++ pair_difference.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int pairDifference(int arr[], int n, int k) {
+    int start=0,end=1;
+
+    while(end<n){
+        int diff=arr[end]-arr[start];
+
+        if(diff==k){
+            cout<<"Pair found: ("<<arr[start]<<", "<<arr[end]<<")"<<endl;
+            return 1;
+        }
+        else if(diff<k){
+            end++;
+        }
+        else{
+            start++;
+        }
+
+        if(start==end){
+            end++;
+        }
+    }
+
+    return 0;
+}
+
+
+int main (){
+    int n,k;
+    cout<<"Enter the size of array and the difference value: ";
+    cin>>n>>k;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    pairDifference(arr,n,k);
+
+
+    return 0;
+}
